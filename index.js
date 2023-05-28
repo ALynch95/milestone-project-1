7aeabe351b75fb55372792316a3e24afc51825e2
Alex <halos959@gmail.com>
2023-05-27 22:47:33 -0700

    Can Place the generated number

--- index.js
+++ index.js
@@ -2,14 +2,29 @@ function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
   
-  const rndNum = randomNumber(1, 1000)
+let rndNum = randomNumber(1, 1000)
 
   function num() { 
     document.getElementById("result").innerHTML = rndNum; 
  } 
 
- let btn = document.getElementById('draw');
+let btn = document.getElementById('draw');
 
  btn.addEventListener('click', function() {
     btn.style.display = 'none';
+    clickListItems();
  })
+
+ function clickListItems() {
+    let listItems = document.querySelectorAll('li');
+    for (let i = 0; i < listItems.length; i++) {
+        listItems[i].addEventListener('click', placeNumber);
+    }
+ }
+
+ function placeNumber() {
+    if (!this.innerHTML) {
+        let rndNum = document.getElementById('result').innerHTML
+        this.innerHTML = rndNum;
+    }
+ }
